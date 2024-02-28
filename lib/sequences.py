49bd8fc2783b4a616529fd89d9f0e2a900727932
dkiriinya <dkiriinya06sc@gmail.com>
2024-02-28 12:04:30 +0300

    learnt common sequesnce operations using fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_array = []
+
+    for i in range(length):
+        if i < 2:
+            fibonacci_array.append(i)
+        else:
+            fibonacci_array.append(fibonacci_array[i-1] + fibonacci_array[i-2])
+    print(fibonacci_array)
+
+print_fibonacci(14)
